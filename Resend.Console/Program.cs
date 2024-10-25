6d0a25d81159d600d28340f2bad044488e589045
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-10-25 18:22:01 -0500

    Add console project to test

---
+++ Resend.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Resend;
+
+var services = new ServiceCollection();
+
+services.AddOptions();
+services.Configure<ResendClientOptions>( o =>
+{
+    o.ApiToken = Environment.GetEnvironmentVariable( "RESEND_APITOKEN" )!;
+} );
+services.AddHttpClient<ResendClient>();
+services.AddTransient<IResend, ResendClient>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+// Resolve and run the service
+var resend = serviceProvider.GetService<IResend>();
+
+var result = await resend.ApiKeyListAsync();
+
+if ( result != null )
+{
+    Console.WriteLine( result.Content.Count() );
+}
